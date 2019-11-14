d3b6f24d26f79f130d497eba49cc47dce16f2969
NickLatkovich <nicklatkovich@gmail.com>
2019-11-14 13:55:17 +0300

    Rm "contract_internal_create_operation" parser

--- src/modules/parser/operations/operation.manager.ts
+++ src/modules/parser/operations/operation.manager.ts
@@ -56,7 +56,6 @@ import { dateFromUtcIso } from '../../../utils/format';
 import { IBlock } from '../../../interfaces/IBlock';
 import BlockRewardOperation from './block.reward.operation';
 import { inspect } from 'util';
-import ContractInternalCreateOperaiton from './contract.internal.create.operation';
 
 type OperationsMap = { [x in ECHO.OPERATION_ID]?: AbstractOperation<x> };
 
@@ -86,7 +85,6 @@ export default class OperationManager {
 		balanceUnfreezeOperation: BalanceUnfreezeOperation,
 		contractCreateOperation: ContractCreateOperation,
 		contractCallOperation: ContractCallOperation,
-		contractInternalCreateOperation: ContractInternalCreateOperaiton,
 		balanceClaimOperation: BalanceClaimOperation,
 		overrideTransferOperation: OverrideTransferOperation,
 		committeeMemberUpdateGlobalParametersOperation: CommitteeMemberUpdateGlobalParametersOperation,
@@ -126,7 +124,6 @@ export default class OperationManager {
 			balanceUnfreezeOperation,
 			contractCreateOperation,
 			contractCallOperation,
-			contractInternalCreateOperation,
 			assetIssueOperation,
 			assetReserveOperation,
 			assetFundFeePoolOperation,
