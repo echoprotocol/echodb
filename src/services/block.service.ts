da0482bcf05ed3ec0a8059e14496f4a11695e62d
kosandrey <kos.andrey@tut.by>
2020-03-27 11:08:15 +0300

    add calculate delegation percent in current period

--- src/services/block.service.ts
+++ src/services/block.service.ts
@@ -37,7 +37,7 @@ export default class BlockService {
 	}
 
 
-	async calculateDelegationRate(blocks: Array<IBlock>) {
+	calculateDelegationRate(blocks: Array<IBlock>) {
 		const blocksCount = blocks.length;
 		const blocksWithDelegateProduser = blocks.filter((b) => b.delegate !== ZERO_ACCOUNT).length;
 		const delegatePercent = (blocksWithDelegateProduser / blocksCount) * 100;
@@ -46,18 +46,39 @@ export default class BlockService {
 
 	async getDelegationRate(opts?: delegateCalcOpts) {
 		const blocks = await this.blockRepository.find({});
-		const delegatePercent = await this.calculateDelegationRate(blocks);
-		const delegationRates = {
-			totalDelegationRate: delegatePercent,
-		};
+		const delegatePercent = this.calculateDelegationRate(blocks);
 		if (opts) {
-			const filtered = blocks.filter((block) => {
+			const startDate = Number(opts.startDate);
+			const endDate = Number(opts.endDate);
+			const interval = Number(opts.interval);
+			if (endDate <= startDate) {
+				throw new Error('Start date is bigger then end date');
+			}
+			if (endDate - startDate < interval) {
+				throw new Error('The choosen period is smaller then interval');
+			}
+			const newMap: Map<number, Array<IBlock>> = new Map();
+			const orderedBlocks = blocks.filter((block) => {
 				const blockTimestamp = Date.parse(block.timestamp) / 1000
-				return blockTimestamp > Number(opts.startDate) && blockTimestamp < Number(opts.endDate)
-			});
+				return (blockTimestamp >= startDate) && (blockTimestamp <= endDate);
+			}).reduce((acc: Map<number, Array<IBlock>>, val: IBlock) => {
+				const timestamp = Date.parse(val.timestamp) / 1000;
+				const segmentNumber = Math.ceil((timestamp - startDate) / interval);
+				return acc.set(segmentNumber, acc.get(segmentNumber) ? [...acc.get(segmentNumber), val] : [val]);
+			}, newMap);
 
+			const ratesMap: Map<string, number> = new Map();
+			for(const blocks of orderedBlocks) {
+				const rate = this.calculateDelegationRate(blocks[1]);
+				const startIntervalDate = startDate + (interval * (blocks[0] - 1));
+				const startIntarelDateString = new Date(startIntervalDate * 1000).toISOString();
+				ratesMap.set(startIntarelDateString, rate)
+			}
+			return {
+				delegatePercent,
+				ratesMap,
+			};
 		}
-
-		return delegationRates;
+		return delegatePercent;
 	}
 }
