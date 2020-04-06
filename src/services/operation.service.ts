aa82ec8e4c2ecbd0d77541540b8f6529d1f2a5c6
kosAndre22 <a.kosobuckiy@pixelplex.io>
2020-04-06 14:36:08 +0300

    fix lint

--- src/services/operation.service.ts
+++ src/services/operation.service.ts
@@ -73,11 +73,17 @@ export default class OperationService {
 		return { total, items };
 	}
 
-	async getSubjectOperations(count: number, offset: number, subject: string, sortDestination: API.SORT_DESTINATION, relationsSubjects?: string[]) {
+	async getSubjectOperations(
+		count: number,
+		offset: number,
+		subject: string,
+		sortDestination: API.SORT_DESTINATION,
+		relationsSubjects?: string[],
+	) {
 		type query = {
 			'_relation.from'?: Object,
 			'_relation.to'?: Object,
-		}
+		};
 		const fromQuery: query = {
 			'_relation.from': { $in: [subject] },
 		};
@@ -89,8 +95,14 @@ export default class OperationService {
 			toQuery['_relation.from'] = { $in: relationsSubjects };
 		}
 		const [fromOperations, toOperations, totalFrom, totalTo] = await Promise.all([
-			this.operationRepository.find(fromQuery, null, { limit: count + offset, sort: { timestamp: sortDestination } }),
-			this.operationRepository.find(toQuery, null, { limit: count + offset, sort: { timestamp: sortDestination } }),
+			this.operationRepository.find(fromQuery, null, {
+				limit: count + offset,
+				sort: { timestamp: sortDestination },
+			}),
+			this.operationRepository.find(toQuery, null, {
+				limit: count + offset,
+				sort: { timestamp: sortDestination },
+			}),
 			this.operationRepository.count(fromQuery),
 			this.operationRepository.count(toQuery),
 		]);
@@ -100,12 +112,12 @@ export default class OperationService {
 		totalOps.sort((op1, op2) => {
 			return sortDestination === 'asc' ?
 				Date.parse(op1.timestamp.toISOString()) - Date.parse(op2.timestamp.toISOString()) :
-				Date.parse(op2.timestamp.toISOString()) - Date.parse(op1.timestamp.toISOString())
+				Date.parse(op2.timestamp.toISOString()) - Date.parse(op1.timestamp.toISOString());
 		});
 		const slicedOps = totalOps.slice(offset, offset + count);
 		return {
 			total: totalCount,
 			items: slicedOps,
-		}
+		};
 	}
 }
