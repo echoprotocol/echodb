48527ad93c6d0f80d89cec5cd261352f57577d24
Andrey Kosobutskiy <a.kosobuckiy@pixelplex.io>
2020-04-06 17:46:23 +0300

    [EDB-196] Add api method to receive all subject ops

--- src/services/operation.service.ts
+++ src/services/operation.service.ts
@@ -31,7 +31,7 @@ export default class OperationService {
 
 	constructor(
 		readonly operationRepository: OperationRepository,
-	) {}
+	) { }
 
 	async getHistory(count: number, offset: number, params: GetHistoryParameters) {
 		const query: Query = {};
@@ -73,4 +73,64 @@ export default class OperationService {
 		return { total, items };
 	}
 
+	async getSubjectOperations(
+		count: number,
+		offset: number,
+		subject: string,
+		relationsSubjects?: string[],
+		params?: GetHistoryParameters,
+	) {
+		type query = {
+			'_relation.from'?: Object,
+			'_relation.to'?: Object,
+			'_relation.contracts'?: Object,
+			'_relation.assets'?: Object,
+			'_relation.tokens'?: Object,
+		};
+		const query: Query = {};
+		const fromQuery: query = {
+			'_relation.from': { $in: [subject] },
+		};
+		const toQuery: query = {
+			'_relation.to': { $in: [subject] },
+		};
+		if (relationsSubjects.length) {
+			fromQuery['_relation.to'] = { $in: relationsSubjects };
+			toQuery['_relation.from'] = { $in: relationsSubjects };
+		}
+
+		const mainQuery = [fromQuery, toQuery];
+		if (params.contracts) mainQuery.push({ '_relation.contracts': { $in: params.contracts } });
+		if (params.assets) mainQuery.push({ '_relation.assets': { $in: params.assets } });
+		if (params.tokens) mainQuery.push({ '_relation.tokens': { $in: params.tokens } });
+
+		const accountsQuery = [];
+		if (params.accounts) {
+			accountsQuery.push(
+				{ '_relation.from': { $in: params.accounts } },
+				{ '_relation.to': { $in: params.accounts } },
+				{ '_relation.accounts': { $in: params.accounts } },
+			);
+		}
+
+		const [items, total] = await Promise.all([
+			this.operationRepository.find(accountsQuery.length ?
+				{ $and: [{ $or: accountsQuery }, { $or: mainQuery }] }
+				: { $or: mainQuery },
+				null, {
+					skip: offset,
+					limit: count,
+					sort: { timestamp: params.sort === API.SORT_DESTINATION.ASC ? 1 : -1 },
+				},
+			),
+			this.operationRepository.count({
+				$or: [fromQuery, toQuery],
+			}),
+		]);
+
+		return {
+			total,
+			items,
+		};
+	}
 }
