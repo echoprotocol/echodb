a931d61c938feb17a009c77b615eb31a18887a27
kosAndre22 <a.kosobuckiy@pixelplex.io>
2020-04-07 14:36:17 +0300

    add asset.dynamic to graphQlShema

--- src/modules/api/types/asset.type.ts
+++ src/modules/api/types/asset.type.ts
@@ -4,6 +4,7 @@ import AssetId from './asset.id.type';
 import AssetBitasset from './asset.bitasset.type';
 import { ObjectType, Field } from 'type-graphql';
 import { MongoId } from '../../../types/mongoose';
+import AssetDynamic from './asset.dynamic.type';
 
 @ObjectType()
 export default class Asset {
@@ -14,5 +15,6 @@ export default class Asset {
 	@Field() symbol: string;
 	@Field() precision: number;
 	@Field(() => AssetOptions) options: AssetOptions;
+	@Field(() => AssetDynamic) dynamic: AssetDynamic;
 	@Field(() => AssetBitasset, { nullable: true }) bitasset: AssetBitasset;
 }
