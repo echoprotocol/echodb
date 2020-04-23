949f12e4b7e16a0e78777461724d87d36b474b1f
kosAndre22 <a.kosobuckiy@pixelplex.io>
2020-04-23 09:40:51 +0300

    fix

--- src/modules/parser/operations/sidechain.btc.create.address.operation.ts
+++ src/modules/parser/operations/sidechain.btc.create.address.operation.ts
@@ -1,5 +1,6 @@
 import AbstractOperation from './abstract.operation';
 import * as ECHO from '../../../constants/echo.constants';
+import EchoRepository from '../../../repositories/echo.repository';
 import { IOperation } from 'interfaces/IOperation';
 
 type OP_ID = ECHO.OPERATION_ID.SIDECHAIN_BTC_CREATE_ADDRESS;
@@ -7,7 +8,9 @@ type OP_ID = ECHO.OPERATION_ID.SIDECHAIN_BTC_CREATE_ADDRESS;
 export default class SidechainBtcCreateAddressOperation extends AbstractOperation<OP_ID> {
 	id = ECHO.OPERATION_ID.SIDECHAIN_BTC_CREATE_ADDRESS;
 
-	constructor() {
+	constructor(
+		private echoRepository: EchoRepository,
+	) {
 		super();
 	}
 
@@ -23,7 +26,8 @@ export default class SidechainBtcCreateAddressOperation extends AbstractOperatio
 		result: Y extends ECHO.KNOWN_OPERATION ? ECHO.OPERATION_RESULT<Y> : unknown,
 	) {
 		const { body } = <IOperation<OP_ID>>operation;
-		body.received_deposit_address = (<any>result).deposit_address.address;
+		const depositAddress = await this.echoRepository.getObject(<any>result);
+		body.received_deposit_address = (<any>depositAddress).deposit_address.address;
 		return <any>body;
 	}
 
