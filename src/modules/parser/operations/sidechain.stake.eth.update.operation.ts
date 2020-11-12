c4fe6ae8de38b194e6f29a6d69c6cbf9bfa0e8ec
kosAndre22 <a.kosobuckiy@pixelplex.io>
2020-11-12 13:31:13 +0300

    fix

--- src/modules/parser/operations/sidechain.stake.eth.update.operation.ts
+++ src/modules/parser/operations/sidechain.stake.eth.update.operation.ts
@@ -1,30 +1,15 @@
 import AbstractOperation from './abstract.operation';
 import * as ECHO from '../../../constants/echo.constants';
-import BalanceRepository from 'repositories/balance.repository';
-import AccountRepository from 'repositories/account.repository';
-import AssetRepository from 'repositories/asset.repository';
 type OP_ID = ECHO.OPERATION_ID.SIDECHAIN_STAKE_ETH_UPDATE;
 
 export default class SidechainStakeEthUpdateOperation extends AbstractOperation<OP_ID> {
 	id = ECHO.OPERATION_ID.SIDECHAIN_STAKE_ETH_UPDATE;
 
-	constructor(
-		private accountRepository: AccountRepository,
-		private assetRepository: AssetRepository,
-		private balanceRepository: BalanceRepository,
-	) {
+	constructor() {
 		super();
 	}
 
 	async parse(body: ECHO.OPERATION_PROPS<OP_ID>) {
-		const account = await this.accountRepository.findById(body.account);
-		const asset = await this.assetRepository.findById(ECHO.SETH_ASSET);
-		await this.balanceRepository.updateOrCreateByAccountAndAsset(
-			account._id,
-			asset._id,
-			body.current_balance.toString(),
-			{ append: true },
-		);
 		return this.validateRelation({
 			from: [body.account],
 			assets: [body.fee.asset_id, body.asset_id],
